f1e2e1584a7d14b31dc5212f376dbde8afa13486
Rahmat Tri Yunandar <devl3@fastratabuana.co.id>
2022-05-17 21:20:29 +0700

    feature implement template web bootstrap to CI

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -5,23 +5,64 @@ class mahasiswa extends CI_Controller
     {
         parent::__construct();
         //Statment
+        $this->load->library(array('template', 'pagination', 'form_validation', 'upload'));
         $this->load->model('m_mahasiswa');
     }
+
+    // function index()
+    // {
+    //     $data['mahasiswa'] = $this->m_mahasiswa->tampilData()->result();
+    //     //print_r($data['mahasiswa']);
+    //     //exit();
+    //     $this->load->view('mahasiswa', $data);
+    // }
 
     function index()
     {
         $data['mahasiswa'] = $this->m_mahasiswa->tampilData()->result();
-        //print_r($data['mahasiswa']);
-        //exit();
-        $this->load->view('mahasiswa', $data);
+        $config['base_url'] = site_url('mahasiswa/index/');
+
+        if ($this->uri->segment(3) == "delete_success")
+            $data['message'] = "<div class='alert alert-success'>Data berhasil dihapus</div>";
+        else if ($this->uri->segment(3) == "add_success")
+            $data['message'] = "<div class='alert alert-success'>Data Berhasil disimpan</div>";
+        else
+            $data['message'] = '';
+        $this->template->display('mahasiswa/index', $data);
     }
 
-    function tambah(){
-        $this->load->helper('url');
-        $this->load->view('input_mahasiswa.php');
+    // function tambah()
+    // {
+    //     $this->load->helper('url');
+    //     $this->load->view('input_mahasiswa.php');
+    // }
+
+    function tambah()
+    {
+        $nim = $this->input->post('nim');
+        if ($nim <> "") {
+            $cek = $this->m_mahasiswa->cek($nim);
+            if ($cek->num_rows() > 0) {
+                $data['message'] = "<div class='alert alert-warning'>NIM sudah digunakan</div>";
+                $this->template->display('mahasiswa/tambah', $data);
+            } else {
+                $isidata = array(
+                    'nim' => $this->input->post('nim'),
+                    'nama' => $this->input->post('nama'),
+                    'jurusan' => $this->input->post('jurusan'),
+                    'alamat' => $this->input->post('alamat')
+                );
+                $this->m_mahasiswa->simpan($isidata);
+                redirect('mahasiswa/index/add_success');
+            }
+        } else {
+            $data['message'] = "";
+            $this->template->display('mahasiswa/tambah', $data);
+        }
     }
 
-    function insert(){
+    function insert()
+    {
         $nim = $this->input->post('nim');
         $nama = $this->input->post('nama');
         $jurusan = $this->input->post('jurusan');
@@ -36,13 +77,40 @@ class mahasiswa extends CI_Controller
 
         $this->m_mahasiswa->save($data);
         redirect('mahasiswa/index');
-
     }
 
-
-    function delete($nim){
-        $this->m_mahasiswa->delete($nim);
-        redirect('mahasiswa/index');
+    function edit($id) //123
+    {
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $jurusan = $this->input->post('jurusan');
+        $alamat = $this->input->post('alamat');
+        if ($nim <> "" and $nama <> "" and $jurusan <> "" and $alamat <> "") {
+            $isidata = array(
+                'nama' => $this->input->post('nama'),
+                'jurusan' => $this->input->post('jurusan'),
+                'alamat' => $this->input->post('alamat')
+            );
+            $this->m_mahasiswa->update($nim, $isidata);
+            $data['message'] = "<div class='alert alert-success'>Data Berhasil diupdate</div>";
+            $data['mhs'] = $this->m_mahasiswa->cek($id)->row_array();
+            $this->template->display('mahasiswa/edit', $data);
+        } else {
+            $data['mhs'] = $this->m_mahasiswa->cek($id)->row_array();
+            $data['message'] = "";
+            $this->template->display('mahasiswa/edit', $data);
+        }
     }
 
+    // function delete($nim)
+    // {
+    //     $this->m_mahasiswa->delete($nim);
+    //     redirect('mahasiswa/index');
+    // }
+
+    function hapus()
+    {
+        $nim = $this->input->post('kode');
+        $this->m_mahasiswa->hapus($nim);
+    }
 }
