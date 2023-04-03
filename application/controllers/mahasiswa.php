64c70ddea841b7dc82e969ba3e2c438c303b0ff6
sucitrasahara <sucitra.scr@bsi.ac.id>
2023-04-03 21:08:49 +0700

    pertemuan 4 MVC + DB CRUD

---
+++ application/controllers/mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+class mahasiswa extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_mahasiswa');
+    }
+    function index()
+    {        
+        $data['mahasiswa'] = $this -> m_mahasiswa->tampilData()->result();
+        //var_dump($data['mahasiswa']);
+        //exit();
+        $this->load->view('mahasiswa', $data);
+    }
+
+    function hapus($nim){
+        $this->m_mahasiswa->hapus($nim);
+
+        $data['mahasiswa'] = $this -> m_mahasiswa->tampilData()->result();
+        $this->load->view('mahasiswa', $data);
+
+    }
+
+}
